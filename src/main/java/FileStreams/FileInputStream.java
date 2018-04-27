4332b533fe3a396cdb1b1768cfec9572985b0307
ilia <38052676+iliaKulakov@users.noreply.github.com>
2018-04-27 23:44:57 +0300

    Created class for input stream

---
+++ src/main/java/FileStreams/FileInputStream.java
@@ -0,0 +1,16 @@
+package FileStreams;
+
+public class FileInputStream {
+
+    String FilePath="D:/Books/Files/";
+
+
+    //планирую задавать с меню путь к файлам
+    public String getFilePath() {
+        return FilePath;
+    }
+
+    public void setFilePath(String filePath) {
+        FilePath = filePath;
+    }
+}
