3d4c1710b2db38f578f1c04d7090da1e343105ac
ilia <38052676+iliaKulakov@users.noreply.github.com>
2018-04-30 08:01:00 +0300

    added IStream

---
+++ src/main/java/menu/Scanner.java
@@ -0,0 +1,10 @@
+package menu;
+
+public class Scanner {
+
+    public void getIntScanner () {
+    java.util.Scanner scName = new java.util.Scanner(System.in);
+     int intVar = scName.nextInt();
+    }
+
+}
